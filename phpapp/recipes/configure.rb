3511ccaea72d6df00b5bd3c0f9b9affa77daf881
hirenpatel <hirenpatel@mac.com>
2013-06-22 19:14:10 +1000

    changes

--- phpapp/recipes/configure.rb
+++ phpapp/recipes/configure.rb
@@ -1,30 +1,6 @@
 node[:deploy].each do |application, deploy|
-
-Chef::Log.debug("start phpapp configure.rb")
-Chef::Log.debug("#{deploy[:deploy_to]}/current/db-connect.php")
-  template "#{deploy[:deploy_to]}/current/db-connect.php" do
-    source "db-connect.php.erb"
-    mode 0660
-    group deploy[:group]
-
-    if platform?("ubuntu")
-      owner "www-data"
-    elsif platform?("amazon")   
-      owner "apache"
-    end
-
-    variables(
-      :host =>     "host_name_here",
-      :user =>     "user_here",
-      :password => "pass_here",
-      :db =>       "database_here",
-      :table =>    "table_here"
-    )
-
-   only_if do
-     File.exists?("#{deploy[:deploy_to]}/current")
-   end
-  end
-Chef::Log.debug("end phpapp configure.rb")
+    Chef::Log.debug("start phpapp configure.rb")
+    Chef::Log.info "start phpapp configure.rb"
+    Chef::Log.debug("{default[:deploy][application][:absolute_document_root]}")
+    Chef::Log.info "{default[:deploy][application][:absolute_document_root]}"
 end
-
