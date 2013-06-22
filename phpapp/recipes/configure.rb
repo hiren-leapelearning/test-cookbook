391269d3057cf58142dc7fb6a8bad0c383b38e3a
hirenpatel <hirenpatel@mac.com>
2013-06-22 19:23:16 +1000

    changes

--- phpapp/recipes/configure.rb
+++ phpapp/recipes/configure.rb
@@ -1,6 +1,6 @@
 node[:deploy].each do |application, deploy|
     Chef::Log.debug("start phpapp configure.rb")
     Chef::Log.info "start phpapp configure.rb"
-    Chef::Log.debug("{default[:deploy][application][:absolute_document_root]}")
-    Chef::Log.info "{default[:deploy][application][:absolute_document_root]}"
+    Chef::Log.debug("#{default[:deploy][application][:absolute_document_root]}")
+    Chef::Log.info "#{default[:deploy][application][:absolute_document_root]}"
 end
