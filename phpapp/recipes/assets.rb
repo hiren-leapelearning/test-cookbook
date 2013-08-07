376a4df4a272c1fc332f6a2fe357b4b3c4d32b9c
Hiren Patel <hiren.patel@leapelearning.com>
2013-08-07 17:36:37 +1000

    set assets

--- phpapp/recipes/assets.rb
+++ phpapp/recipes/assets.rb
@@ -2,26 +2,27 @@ node[:deploy].each do |application, deploy|
   Chef::Log.debug("start phpapp assets.rb")
   Chef::Log.debug("#{node[:deploy][application][:deploy_to]}/current")
   Chef::Log.debug("#{node[:environment]}")
-  template "#{node[:deploy][application][:deploy_to]}/current/application_lms/config/database.php" do
-    source "#{node[:deploy][application][:deploy_to]}/current/utilities/deploy-files/lms/#{node[:environment]}/database.php"
+  Chef::Log.debug("#{current_release}")
+  template "#{current_release}/application_lms/config/database.php" do
+    source "#{current_release}/utilities/deploy-files/lms/#{node[:environment]}/database.php"
   end
-  template "#{node[:deploy][application][:deploy_to]}/current/application_lms/config/config.php" do
-    source "#{node[:deploy][application][:deploy_to]}/current/utilities/deploy-files/lms/#{node[:environment]}/config.php"
+  template "#{current_release}/application_lms/config/config.php" do
+    source "#{current_release}/utilities/deploy-files/lms/#{node[:environment]}/config.php"
   end
-  template "#{node[:deploy][application][:deploy_to]}/current/application_lms/config/zoho.php" do
-    source "#{node[:deploy][application][:deploy_to]}/current/utilities/deploy-files/lms/#{node[:environment]}/zoho.php"
+  template "#{current_release}/application_lms/config/zoho.php" do
+    source "#{current_release}/utilities/deploy-files/lms/#{node[:environment]}/zoho.php"
   end
-  template "#{node[:deploy][application][:deploy_to]}/current/application_lms/config/xero.php" do
-    source "#{node[:deploy][application][:deploy_to]}/current/utilities/deploy-files/lms/#{node[:environment]}/xero.php"
+  template "#{current_release}/application_lms/config/xero.php" do
+    source "#{current_release}/utilities/deploy-files/lms/#{node[:environment]}/xero.php"
   end
-  template "#{node[:deploy][application][:deploy_to]}/current/public_lms/index.php" do
-    source "#{node[:deploy][application][:deploy_to]}/current/utilities/deploy-files/lms/#{node[:environment]}/index.php"
+  template "#{current_release}/public_lms/index.php" do
+    source "#{current_release}/utilities/deploy-files/lms/#{node[:environment]}/index.php"
   end
-  template "#{node[:deploy][application][:deploy_to]}/current/public_lms/.htaccess" do
-    source "#{node[:deploy][application][:deploy_to]}/current/utilities/deploy-files/lms/#{node[:environment]}/.htaccess"
+  template "#{current_release}/public_lms/.htaccess" do
+    source "#{current_release}/utilities/deploy-files/lms/#{node[:environment]}/.htaccess"
   end
-  template "#{node[:deploy][application][:deploy_to]}/current/utilities/ruckusing/config/database.inc.php" do
-    source "#{node[:deploy][application][:deploy_to]}/current/utilities/deploy-files/lms/#{node[:environment]}/database.inc.php"
+  template "#{current_release}/utilities/ruckusing/config/database.inc.php" do
+    source "#{current_release}/utilities/deploy-files/lms/#{node[:environment]}/database.inc.php"
   end
 Chef::Log.debug("end phpapp assets.rb")
 Chef::Log.info "end phpapp assets.rb"
